ef4baea5aee9faa16381f1252a8358e3f37f923b
pkumars397 <pkumars397@gmail.com>
2023-08-08 19:19:44 +0530

    Pattern 1

---
+++ Looping/Patterns/Pattern 1.js
@@ -0,0 +1,19 @@
+/*
+   *****
+
+   ****
+
+   ***
+
+   **
+
+   *
+  
+   */
+for (var i = 1; i <= 5; i++) {
+  var str = "";
+  for (var j = 1; j <= 6 - i; j++) {
+    str += "*";
+  }
+  console.log(str);
+}
