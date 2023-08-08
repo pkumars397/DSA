6566ea114f23d6a3c9b0fc1c65b04f7673b865ac
pkumars397 <pkumars397@gmail.com>
2023-08-08 20:43:08 +0530

    SumOfOddLengthSubArray

---
+++ Array/Sum of Odd length Subarrays.js
@@ -0,0 +1,28 @@
+var sumOddLengthSubarrays = function(arr) {
+    var sum = 0;
+   
+    // Size of array
+    var l = arr.length;
+   
+    // Traverse the array
+    for(var i = 0; i < l; i++)
+    {
+          
+        // Generate all subarray of
+        // odd length
+        for(var j = i; j < l; j += 2)
+        {
+            for(var k = i; k <= j; k++)
+            {
+                  
+                // Add the element to sum
+                sum += arr[k];
+            }
+        }
+    }
+    // Return the final sum
+    return sum;
+};
+
+var a = [1, 2, 35, 6];
+console.log(sumOddLengthSubarrays(a))
