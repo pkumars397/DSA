ca322fb351e86f4b561a191aa62eb233881e0a5f
pkumars397 <pkumars397@gmail.com>
2023-08-08 19:25:13 +0530

    Pattern 2

---
+++ Looping/Patterns/Pattern 2 Using While.js
@@ -0,0 +1,42 @@
+// *****
+
+// ****
+
+// ***
+
+// **
+
+// *
+
+// *
+
+// **
+
+// ***
+
+// ****
+
+// *****
+
+var i=1;
+    while(i<=5){
+        var str="";
+        var j=10;
+        while(j<=15-i){
+            str+="*";
+            j++;
+        }
+        console.log(str);
+        i++;
+    }
+    var i=6;
+    while(i<=10){
+        var str="";
+        var j=10;
+        while(j<=4+i){
+            str+="*";
+            j++;
+        }
+        console.log(str);
+        i++;
+    }
---
+++ Looping/Patterns/Pattern 2.js
@@ -0,0 +1,36 @@
+// *****
+
+// ****
+
+// ***
+
+// **
+
+// *
+
+// *
+
+// **
+
+// ***
+
+// ****
+
+// *****
+
+for(var i=1;i<=5;i++){
+    var str="";
+    for(var j=1;j<=6-i;j++){
+            
+            str+="*";
+        }
+        console.log(str);
+    }
+    for(var i=6;i<=10;i++){
+    var str="";
+    for(var j=6;j<=i;j++){
+            
+            str+="*";
+        }
+        console.log(str);
+    }
