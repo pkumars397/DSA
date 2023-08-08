ad37111f829c203fe2ff23822ce5453eb75b9911
pkumars397 <pkumars397@gmail.com>
2023-08-08 20:52:05 +0530

    MissingNumber

---
+++ Array/MissingNumber.js
@@ -0,0 +1,14 @@
+var missing_elements = function(arr) {
+    
+    var l=arr.length+1;
+    var sumN;
+    sumN=l*(l+1)/2;
+    var sum=0;
+    for(var i=0;i<arr.length;i++){
+        sum+=arr[i];
+    }
+    return sumN-sum;
+};
+
+var arr = [3, 4, 1];
+console.log(missing_elements(arr));
