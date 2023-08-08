31627d21e1b79612445c018ebe4aced5b68a5251
pkumars397 <pkumars397@gmail.com>
2023-08-08 22:50:46 +0530

    selectionSort

---
+++ Array/SelectionSort.js
@@ -0,0 +1,19 @@
+function selectionSort(arr) {
+  var n = arr.length;
+  for (var i = 0; i < n; i++) {
+    var max = i;
+    for (var j = i + 1; j < n; j++) {
+      if (arr[j] > arr[max]) {
+        max = j;
+      }
+    }
+
+    if (max != i) {
+      [arr[i], arr[max]] = [arr[max], arr[i]];
+    }
+  }
+  return arr;
+}
+
+var arr = [5, 1, 2, 4, 3];
+console.log(selectionSort(arr));
