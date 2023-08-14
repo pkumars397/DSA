74800356a0cbf868a3e803751bbe2ca6576350e4
pkumars397 <pkumars397@gmail.com>
2023-08-14 15:58:38 +0530

    Prime number in o(n)

--- Looping/Amrstrong Number.js
+++ Looping/Amrstrong Number.js
@@ -1,20 +1,19 @@
 function armStrong(n) {
-    var temp = n;
-    var count = Math.abs(n).toString().length;
-    var sum = 0;
-    while (n > 0) {
-        var digit = n % 10;
-        sum += Math.pow(digit, count);
-        n = Math.floor(n / 10);
-    }
+  var temp = n;
+  var count = Math.abs(n).toString().length;
+  var sum = 0;
+  while (n > 0) {
+    var digit = n % 10;
+    sum += Math.pow(digit, count);
+    n = Math.floor(n / 10);
+  }
 
-    if (sum == temp) {
-        return true;
-    }
-    else {
-        return false;
-    }
+  if (sum == temp) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 var n = 153;
-console.log(armStrong(n))
+console.log(armStrong(n));
