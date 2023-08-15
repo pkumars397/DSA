9379b37617b2978ba305cf89181faf7d426a6f0b
pkumars397 <pkumars397@gmail.com>
2023-08-15 09:29:13 +0530

    extract numbers using map

--- Bit_Manipulation/Extracting Numbers.js
+++ Bit_Manipulation/Extracting Numbers.js
@@ -1,26 +1,41 @@
-var extract_number = function(s) {
-    var a=[];
-    for(var i=0;i<s.length;){
-        if(s.charCodeAt(i)>=48 && s.charCodeAt(i)<=57){
-           var j=i;
-           while(j<s.length && s.charCodeAt(j)>=48 && s.charCodeAt(j)<=57){
-               j++;
-           }
+// var extract_number = function(s) {
+//     var a=[];
+//     for(var i=0;i<s.length;){
+//         if(s.charCodeAt(i)>=48 && s.charCodeAt(i)<=57){
+//            var j=i;
+//            while(j<s.length && s.charCodeAt(j)>=48 && s.charCodeAt(j)<=57){
+//                j++;
+//            }
            
-           a.push(parseInt(s.substring(i,j )));
+//            a.push(parseInt(s.substring(i,j )));
            
-           i=j;
+//            i=j;
          
-         }
-         else{
-             i=i+1;
-        }
+//          }
+//          else{
+//              i=i+1;
+//         }
         
-    }
-    return a;
+//     }
+//     return a;
     
      
+// };
+
+
+var extract_number=function(s){
+    let map = new Map()
+    for (let i = 0; i < s.length; i++) {
+        if (s.charCodeAt(i) >= 48 && s.charCodeAt(i) <= 57) {
+            map.set(i, s[i])
+        }
+    }
+    var s = '';
+    for (val of map.values()) {
+        s += val;
+    }
+    return s;
 };
 
-var s = "hsleo469dj4e"
+let s = "hsleo469dj4e"
 console.log(extract_number(s));
