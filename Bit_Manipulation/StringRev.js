431d222afd448544189cdca33a225465828734cd
pkumars397 <pkumars397@gmail.com>
2023-08-16 12:58:41 +0530

    string reverse using stack

--- Bit_Manipulation/StringRev.js
+++ Bit_Manipulation/StringRev.js
@@ -12,12 +12,25 @@ var isValid = function (s) {
 
   //Method 2
 
-  var rev = s.split("").reverse().join("");
-  if (rev == s) {
+  // var rev = s.split("").reverse().join("");
+  // if (rev == s) {
+  //   return true;
+  // }
+  // return false;
+  // return rev;
+
+  // Method 3
+
+  let rev = s.split("");
+  const l = s.length;
+  let res = [];
+  for (let i = 0; i < l; i++) {
+    res.push(rev.pop());
+  }
+  if (res.join("") == s) {
     return true;
   }
   return false;
-  return rev;
 };
-
-console.log(isValid("madam"));
+let s = "madama";
+console.log(isValid(s));
