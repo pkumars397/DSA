d959951f7d4d8e30eaaf8b324d73686d9835757f
pkumars397 <pkumars397@gmail.com>
2023-08-21 09:53:52 +0530

    sliding window max sum subarray

---
+++ Stacks and Queues/Sliding window Max sum of subarray.js
@@ -0,0 +1,25 @@
+let a = [1, 2, 3, 1, 4, 5, 2, 3, 6];
+let k = 3;
+
+var maxSum = function (a,k) {
+    let i = 0, j = 0;
+    let max = 0;
+    let sum = 0;
+        
+        while (j < a.length) {
+            sum = sum + a[j]
+            if (j - i + 1 < k) {
+                j++;
+            }
+            else if(j-i+1==k){
+                max = Math.max(sum, max);
+                sum = sum - a[i];
+            }
+            i++;
+            j++;
+        }
+        
+    console.log(max);
+}
+
+maxSum(a,k)
