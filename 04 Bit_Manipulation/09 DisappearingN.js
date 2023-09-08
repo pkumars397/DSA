aaba0542bdb77195f8359d6be27fbca715aa95f7
pkumars397 <pkumars397@gmail.com>
2023-09-08 10:50:50 +0530

    updated

--- 04 Bit_Manipulation/09 DisappearingN.js
+++ 04 Bit_Manipulation/09 DisappearingN.js
@@ -1,22 +1,33 @@
-var missing_elements = function(arr) {
-    var b=[];
-    for(var i=0;i<arr.length;i++){
-        b.push(0);
-    }
+// var missing_elements = function(arr) {
+//     var b=[];
+//     for(var i=0;i<arr.length;i++){
+//         b.push(0);
+//     }
     
-    for(var i=0;i<arr.length;i++){
-        var correct_index=arr[i]-1;
-        b[correct_index]=arr[i];
+//     for(var i=0;i<arr.length;i++){
+//         var correct_index=arr[i]-1;
+//         b[correct_index]=arr[i];
         
-    }
-    var ans=[];
-    for(var i=0;i<arr.length;i++){
-        if(b[i]==0){
-            ans.push(i+1)
-        }
-    }
-    return ans;
-};
+//     }
+//     var ans=[];
+//     for(var i=0;i<arr.length;i++){
+//         if(b[i]==0){
+//             ans.push(i+1)
+//         }
+//     }
+//     return ans;
+// };
+
+// function missing_elements(array) {
+//     let set = new Set(array);
+//     let res=[];
+//     for (let i = 1; i <= array.length; i++){
+//         if (!set.has(array[i])) {
+//             res.push(i);
+//         }
+//     }
+//     return res;
+// }
 
 var array = [1, 1]
 console.log(missing_elements(array))
