1ca4677c29cb397c1b3e6798aa3f33e854c50819
pkumars397 <pkumars397@gmail.com>
2023-09-12 11:40:13 +0530

    updated Armstrong Number

--- 02 Looping/04 Amrstrong Number.js
+++ 02 Looping/04 Amrstrong Number.js
@@ -1,19 +1,27 @@
 function armStrong(n) {
-  var temp = n;
-  var count = Math.abs(n).toString().length;
-  var sum = 0;
-  while (n > 0) {
-    var digit = n % 10;
-    sum += Math.pow(digit, count);
-    n = Math.floor(n / 10);
-  }
+  // var temp = n;
+  // var count = Math.abs(n).toString().length;
+  // var sum = 0;
+  // while (n > 0) {
+  //   var digit = n % 10;
+  //   sum += Math.pow(digit, count);
+  //   n = Math.floor(n / 10);
+  // }
 
-  if (sum == temp) {
-    return true;
-  } else {
-    return false;
+  // if (sum == temp) {
+  //   return true;
+  // } else {
+  //   return false;
+  // }
+  let str = String(n);
+  let noOfDigits = str.length;
+  let sum = 0;
+  for (let i = 0; i < noOfDigits; i++){
+    let digit = parseInt(str[i]);
+    sum+=Math.pow(digit,noOfDigits)
   }
+  return sum === n;
 }
 
-var n = 153;
+var n = 133;
 console.log(armStrong(n));
