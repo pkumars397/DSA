8a2789e4e4f15d40e63cdc9b2c1bc8be4c190c28
pkumars397 <pkumars397@gmail.com>
2023-09-26 13:33:08 +0530

    updated

--- 04 Bit_Manipulation/07 SortingByBits.js
+++ 04 Bit_Manipulation/07 SortingByBits.js
@@ -1,35 +1,56 @@
-var sortByBits = function (arr) {
-    //    Counting the setBit and storing into sort array
-    var b = [];
-    for (var i = 0; i < arr.length; i++) {
-        var setBit = 0;
+// var sortByBits = function (arr) {
+//     //    Counting the setBit and storing into sort array
+//     var b = [];
+//     for (var i = 0; i < arr.length; i++) {
+//         var setBit = 0;
             
-        for (var j = 0; j < 32; j++) {
-            if (arr[i] >> j & 1 === 1) {
-                setBit++
-            }
-        }
-        b.push(setBit)
-    }
-    // Applying bubble sort for sorting both array
-    for (var i = 0; i < arr.length; i++) {
-        for (var j = 0; j < arr.length; j++) {
-            if (b[j] > b[j + 1] || b[j] == b[j + 1] && arr[j] > arr[j + 1]) {
-                var temp = b[j];
-                b[j] = b[j + 1];
-                b[j + 1] = temp;
+//         for (var j = 0; j < 32; j++) {
+//             if (arr[i] >> j & 1 === 1) {
+//                 setBit++
+//             }
+//         }
+//         b.push(setBit)
+//     }
+//     // Applying bubble sort for sorting both array
+//     for (var i = 0; i < arr.length; i++) {
+//         for (var j = 0; j < arr.length; j++) {
+//             if (b[j] > b[j + 1] || b[j] == b[j + 1] && arr[j] > arr[j + 1]) {
+//                 var temp = b[j];
+//                 b[j] = b[j + 1];
+//                 b[j + 1] = temp;
                     
-                var temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
+//                 var temp = arr[j];
+//                 arr[j] = arr[j + 1];
+//                 arr[j + 1] = temp;
 
-            }
-        }
-    }
-    return arr;
+//             }
+//         }
+//     }
+//     return arr;
         
-};
+// };
     
+var sortByBits = function (arr){
+    // using built in sort method of array in js
+    arr.sort((a,b)=>{
+        if(count_bits(a)==count_bits(b)){
+            return a-b;
+        }
+        return count_bits(a)-count_bits(b)
+    })
+    return arr;
+    // counting setbits(1) in number.
+    function count_bits(n){
+        let count=0;
+        for(let i=0;i<32;i++){
+           if((n)&1){
+            count++;
+           }
+           n=n>>1;
+        }
+        return count;
+    }
+}
 var array = [];
-array = [5, 3, 7, 10, 2]
+array = [0,1,2,3,4,5,6,7,8,9]
 console.log(sortByBits(array));
--- 04 Bit_Manipulation/09 DisappearingN.js
+++ 04 Bit_Manipulation/09 DisappearingN.js
@@ -1,22 +1,22 @@
-// var missing_elements = function(arr) {
-//     var b=[];
-//     for(var i=0;i<arr.length;i++){
-//         b.push(0);
-//     }
+var missing_elements = function(arr) {
+    var b=[];
+    for(var i=0;i<arr.length;i++){
+        b.push(0);
+    }
     
-//     for(var i=0;i<arr.length;i++){
-//         var correct_index=arr[i]-1;
-//         b[correct_index]=arr[i];
+    for(var i=0;i<arr.length;i++){
+        var correct_index=arr[i]-1;
+        b[correct_index]=arr[i];
         
-//     }
-//     var ans=[];
-//     for(var i=0;i<arr.length;i++){
-//         if(b[i]==0){
-//             ans.push(i+1)
-//         }
-//     }
-//     return ans;
-// };
+    }
+    var ans=[];
+    for(var i=0;i<arr.length;i++){
+        if(b[i]==0){
+            ans.push(i+1)
+        }
+    }
+    return ans;
+};
 
 // function missing_elements(array) {
 //     let set = new Set(array);
